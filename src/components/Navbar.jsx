93870653faedead511c90b0b078778fcec21fda8
shailesh rawat <shailesh.rawat5021@gmail.com>
2023-07-23 12:24:54 +0530

    fixed some errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,13 +23,7 @@ const Navbar = (props) => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-            </ul>
+
             <div className={`form-check form-switch text-${props.mode === 'dark' ? 'light' : 'dark'}`}>
               <input
                 className="form-check-input"
